06924e4b9fc2a08005a93b95c65ed5ede4a0d6f2
vedpatil611 <vedpatil611@gmail.com>
2022-01-20 08:58:58 +0530

    Remove new line character

--- cwd.c
+++ cwd.c
@@ -6,17 +6,21 @@
 
 int main() {
 	struct passwd* wd = getpwuid(getuid());
+	
+	// Current username
 	const char* username = wd->pw_name;
+	// Currnet user home direcotry
 	const char* home_dir = wd->pw_dir;
 
+	// Current working directory
 	const char* current_dir = getenv("PWD");
 
-	if(strcmp(home_dir, current_dir) == 0) 
+	if(strcmp(home_dir, current_dir) == 0)          // If currrent working directory is same as user's home direcotry, print ~
 	{
 		printf("~");
 		return 0;
 	}
-	else if(strcmp(current_dir, "/") == 0)
+	else if(strcmp(current_dir, "/") == 0)         // If user is at root level print /
 	{
 		printf("/");
 		return 0;
@@ -27,13 +31,14 @@ int main() {
 	char* tok = strtok(dir, "/");
 	char* tmp;
 	
-	while(tok != NULL) 
+	while(tok != NULL)                             // Keep tokenizing current dir path until you get current folder name
 	{
 		tmp = tok;
 		tok = strtok(NULL, "/");
 	}
 
-	printf("%s\n", tmp);
+	// print directory name
+	printf("%s", tmp);
 
 	return 0;
 }
